ff6183df3bc56b6cd296c1be982f0d3437e0a26a
emmagan <47680984+emmagan@users.noreply.github.com>
2019-02-16 11:52:47 -0500

    working screenshots of video stream

--- script.js
+++ script.js
@@ -1,80 +1,25 @@
-/*
-var video = document.querySelector("#videoElement");
+var video = document.querySelector('#videoElement');
+var canvas = document.querySelector('#canvas');
+var context = canvas.getContext('2d');
+var rawImData;
 
-if (navigator.mediaDevices.getUserMedia) {       
-	navigator.mediaDevices.getUserMedia({video: true})
+navigator.mediaDevices.getUserMedia({video: true})
   .then(function(stream) {
     video.srcObject = stream;
-  })
-  .catch(function(err0r) {
-    console.log("Something went wrong!");
-  });
-}
-*/
+  }).then(getScreenshot());
 
-let preview = document.getElementById("preview");
-let recording = document.getElementById("recording");
-let startButton = document.getElementById("startButton");
-let stopButton = document.getElementById("stopButton");
-let downloadButton = document.getElementById("downloadButton");
-let logElement = document.getElementById("log");
-
-let recordingTimeMS = 5000;
-function log(msg) {
-  logElement.innerHTML += msg + "\n";
-}
-function wait(delayInMS) {
-  return new Promise(resolve => setTimeout(resolve, delayInMS));
-}
-function startRecording(stream, lengthInMS) {
-  let recorder = new MediaRecorder(stream);
-  let data = [];
- 
-  recorder.ondataavailable = event => data.push(event.data);
-  recorder.start();
-  log(recorder.state + " for " + (lengthInMS/1000) + " seconds...");
- 
-  let stopped = new Promise((resolve, reject) => {
-    recorder.onstop = resolve;
-    recorder.onerror = event => reject(event.name);
-  });
-
-  let recorded = wait(lengthInMS).then(
-    () => recorder.state == "recording" && recorder.stop()
-  );
- 
-  return Promise.all([
-    stopped,
-    recorded
-  ])
-  .then(() => data);
-}
-function stop(stream) {
-  stream.getTracks().forEach(track => track.stop());
+function getScreenshot(){
+  setInterval(function() {
+    context.clearRect(0,0, canvas.width, canvas.height);
+    context.beginPath();
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    download();
+  }, 1000);
 }
 
-startButton.addEventListener("click", function() {
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-  }).then(stream => {
-    preview.srcObject = stream;
-    downloadButton.href = stream;
-    preview.captureStream = preview.captureStream || preview.mozCaptureStream;
-    return new Promise(resolve => preview.onplaying = resolve);
-  }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
-  .then (recordedChunks => {
-    let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
-    recording.src = URL.createObjectURL(recordedBlob);
-    downloadButton.href = recording.src;
-    downloadButton.download = "RecordedVideo.webm";
-    
-    log("Successfully recorded " + recordedBlob.size + " bytes of " +
-        recordedBlob.type + " media.");
-  })
-  .catch(log);
-}, false);
-
-stopButton.addEventListener("click", function() {
-  stop(preview.srcObject);
-}, false);
+function download() {
+  rawImData = canvas.toDataURL("image/jpg;base64");
+  rawImData = rawImData.replace("image/jpg", "image/octet");
+  document.location.href = rawImData;
+  document.body.removeChild(canvas);
+}

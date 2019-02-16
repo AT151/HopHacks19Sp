3ceaed01a741022b559025d6132af80da15f22a6
emmagan <47680984+emmagan@users.noreply.github.com>
2019-02-16 17:15:44 -0500

    read file.txt on a button click

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var video = document.querySelector('#videoElement');
 var canvas = document.querySelector('#canvas');
 var display = document.querySelector("#display");
-
+var logElem = document.querySelector("#log");
 
 var context = canvas.getContext('2d');
 var rawImData; var link; var fileName;
@@ -18,13 +18,12 @@ var intSetter = setInterval(function() {
   context.clearRect(0,0, canvas.width, canvas.height);
   context.beginPath();
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  download();
+  //download();
 }, 1000);
   
 function getScreenshot(){
   intSetter;
 }
-
 
 function stop(stream) {
   stream.getTracks().forEach(track => track.stop());
@@ -45,3 +44,44 @@ function download() {
 stopButton.addEventListener("click", function() {
   stop(video.srcObject);
 }, false)
+
+/*
+function log(msg) {
+  var time = new Date();
+  var timeStr = time.toLocaleTimeString();
+  logElem.innerHTML += timeStr + ": " + msg + "<br/>";
+}
+
+log("Logging mouse events inside this container...");
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'file.txt', true);
+xhr.addEventListener("load", reqListener);
+xhr.send(null);
+
+function logEvent(event) {
+  var msg = xhr.responseTest;
+  log(msg);
+//var msg = "Event <strong>" + event.type;
+  //var msg = xhr.responseTest;
+  //log(msg);
+}
+
+//var boxElem = document.querySelector(".box");
+//boxElem.addEventListener("click", logEvent);
+*/
+
+var http_request = new XMLHttpRequest();
+
+function makeRequest() {
+  http_request.onreadystatechange = handleCheckboxRequest;
+  http_request.open('GET','file.txt',true);
+  http_request.send(null);
+}  
+
+function handleCheckboxRequest() {     
+  if (http_request.readyState == 4 && http_request.status == 200) {
+	logElem.innerHTML = http_request.responseText;
+  }
+}
+
+var textSetter = setInterval(makeRequest(), 1000);  

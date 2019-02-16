2097403bcab4ecdc7f05db078b0916ce40fb8ccc
Anh T. Tran <trantheanh1512000@gmail.com>
2019-02-16 16:02:11 -0500

    added stop functionality

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 var video = document.querySelector('#videoElement');
 var canvas = document.querySelector('#canvas');
+var display = document.querySelector("#display");
+
 
 var context = canvas.getContext('2d');
 var rawImData; var link; var fileName;
+
+let stopButton = document.getElementById("stopButton");
 
 navigator.mediaDevices.getUserMedia({video: true})
   .then(function(stream) {
     video.srcObject = stream;
   }).then(getScreenshot());
 
+
+var intSetter = setInterval(function() {
+  context.clearRect(0,0, canvas.width, canvas.height);
+  context.beginPath();
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  download();
+}, 1000);
+  
 function getScreenshot(){
-  setInterval(function() {
-    context.clearRect(0,0, canvas.width, canvas.height);
-    context.beginPath();
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    //download();
-  }, 1000);
+  intSetter;
+}
+
+
+function stop(stream) {
+  stream.getTracks().forEach(track => track.stop());
+  clearInterval(intSetter);
 }
 
 function download() {
   rawImData = canvas.toDataURL("image/jpeg", 1.0);
   fileName = "download.jpeg";
-  //rawImData = rawImData.replace("image/jpeg", "image/octet");
-  //document.location.href = rawImData;
-  //document.body.removeChild(canvas);
-  
   link = document.createElement('a');
   link.href = rawImData;
   link.download = fileName;
@@ -32,3 +41,7 @@ function download() {
   link.click();
   document.body.removeChild(link);
 }
+
+stopButton.addEventListener("click", function() {
+  stop(video.srcObject);
+}, false)
